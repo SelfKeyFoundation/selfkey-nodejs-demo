38d3f3def74940b881772e5fd16ae66f5f0b6c07
Ben Gervais <ben@grvs.io>
2019-01-11 23:21:55 +0700

    Update lws.js

--- routes/lws.js
+++ routes/lws.js
@@ -1,6 +1,5 @@
 'use strict'
 
-// RP Dependencies
 const HOST = `http://localhost:${process.env.PORT}`
 const express = require('express')
 const router = express.Router()
@@ -14,8 +13,8 @@ const jwt = require('jsonwebtoken')
 const uuid = require('node-uuid')
 const path = require('path')
 
+// development sourced library
 const selfkey = require('../../selfkey-lib/lib/selfkey.js')
-
 
 async function generateToken() {
 	return (new String(uuid.v1() + uuid.v4())).replace(/-/g, '')
@@ -23,15 +22,10 @@ async function generateToken() {
 
 // API
 const login = async (req, res) => {
-	
 	const { body } = req
-	
 	if (!body.token) {
 		return res.status(400).json({ redirectTo: '/login' })
-	}
-
-	console.log(body.token)
-	
+	}	
 	try {
 		let decoded = await jwt.verify(body.token, 'SHHH')
 		console.log(decoded)
@@ -48,10 +42,8 @@ const login = async (req, res) => {
 
 // API
 const createUser = async (req, res) => {
-	
 	let publicKey = req.decodedAuth.sub
 	let attributes = req.body.attributes
-
 	try {
 		attributes = JSON.parse(attributes)
 	} catch (error) {
@@ -60,30 +52,24 @@ const createUser = async (req, res) => {
 			message: 'Attributes field must be a json string'
 		})
 	}
-
 	if (!attributes || !attributes.length) {
 		return res.status(400).json({ code: 'no_attributes', message: 'No attributes provided' })
 	}
-
 	let documents = req.files.map(f => {
-		console.log(f)
-		let uid = f.fieldname.match(/^\$document-([0-9]*)$/) || Math.Random(1,3).toString()
-		console.log(uid)
+		let id = f.fieldname.match(/^\$document-([0-9]*)$/) || Math.Random(1,3).toString()
 		let doc = {
 			mimeType: f.mimetype,
 			size: f.size,
 			content: f.buffer,
 			link: path.join(__dirname, '../', '/public/uploads/', publicKey, '/', f.originalname),
-			uid: uid
+			uid: id
 		}
 		return doc
 	})
-
 	documents = documents.map(doc => {
 		let newDoc = Document.create(doc)
 		return doc
 	})
-
 	attributes = attributes.map(attr => {
 		let attrDocs = attr.documents
 			.map(id => {
@@ -95,70 +81,48 @@ const createUser = async (req, res) => {
 		let { value } = selfkey.denormalizeDocumentsSchema(attr.schema, attr.data, attrDocs)
 		return { id: attr.id, value }
 	})
-
-
 	let user = await User.findOne({selfkey_wallet: publicKey})
-	console.log(user)
-
 	let realToken = await generateToken()
-
 	if (user) {
-		console.log('UPDATE')
 		user = await User.update({selfkey_wallet: publicKey}, { token: realToken, attributes: attributes })
 	} else {
-		console.log('NEW')
 		user = await User.create({selfkey_wallet: publicKey, token: realToken, attributes: attributes})
 	}
-
 	if (!user) {
 		return res.status(400).json({
 			code: 'could_not_create',
 			message: 'Could not create user'
 		})
 	}
-
 	return res.status(201).json()
 }
 
 // API
 const getUserPayload = async (req, res) => {
-	
 	let publicKey = req.decodedAuth.sub
-	console.log(publicKey)
 	let user = await User.findOne({selfkey_wallet: publicKey})
-	console.log(user)
-
 	if (!user) {
 		return res.status(404).json({
 			code: 'user_does_not_exist',
 			message: 'User with provided public key does not exist'
 		})
 	}
-
 	let userToken = jwt.sign({}, 'SHHH', { subject: '' + user.selfkey_wallet })
-
 	return res.status(200).json({ token: userToken })
 }
 
 // ROUTES
 
 router.get('/selfkey/v2/auth/challenge/:publicKey', async (req, res) => {
-	
 	let cr = await selfkey.newJWT(req.params.publicKey)
-	
 	return res.status(cr.status).json(cr)
 })
 
 router.post('/selfkey/v2/auth/challenge', selfkey.jwtAuthMiddleware, async (req, res) => {
-	
 	let challenge = req.decodedAuth.challenge
 	let publicKey = req.decodedAuth.sub
 	let { signature } = req.body || {}
-
 	let cr = await selfkey.handleChallengeResponse(challenge, signature, publicKey)
-
-	console.log(cr)
-
 	return res.status(cr.status).json(cr)
 })
 
