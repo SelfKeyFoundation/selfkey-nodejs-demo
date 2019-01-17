61e20c16ca3e4e376835a1634dfdc8c21131693c
altninja <ben@grvs.io>
2019-01-17 11:16:22 +0700

    remove jwt to lib

--- routes/lws.js
+++ routes/lws.js
@@ -9,10 +9,8 @@ const multer = require('multer')
 const upload = multer({ dest: 'public/uploads/' })
 const User = require('../db/models/lws_user')
 const Document = require('../db/models/lws_doc')
-const jwt = require('jsonwebtoken')
 const uuid = require('node-uuid')
 const path = require('path')
-const JWT_SECRET = process.env.SK_JWT_SECRET
 
 // development sourced library
 const selfkey = require('../../selfkey-lib/lib/selfkey.js')
@@ -28,14 +26,14 @@ const login = async (req, res) => {
 		return res.status(400).json({ redirectTo: '/login' })
 	}	
 	try {
-		let decoded = await jwt.verify(body.token, JWT_SECRET)
+		let decoded = selfkey.verifyJWT(body.token)
 		let user = await User.findOne({selfkey_wallet: decoded.sub})
 		if (!user) {
 			return res.status(404).json({ redirectTo: '/login' })
 		}
 		return res.json({ redirectTo: '/success/' + user.token })
 	} catch (error) {
-		return res.satus(401).json({ redirectTo: `/login` })
+		return res.status(401).json({ redirectTo: `/login` })
 	}
 }
 
@@ -43,32 +41,36 @@ const login = async (req, res) => {
 const createUser = async (req, res) => {
 	let publicKey = req.decodedAuth.sub
 	let attributes = req.body.attributes
+	
+	// LIB?
 	try {
 		attributes = JSON.parse(attributes)
 	} catch (error) {
-		return res.status(400).json({
-			code: 'invalid_attributes',
-			message: 'Attributes field must be a json string'
-		})
+		return res.status(400).json({ code: 'invalid_attributes', message: 'Attributes field must be a json string' })
 	}
 	if (!attributes || !attributes.length) {
 		return res.status(400).json({ code: 'no_attributes', message: 'No attributes provided' })
 	}
+	
+	// Express specific Multer handling
 	let documents = req.files.map(f => {
 		let id = f.fieldname.match(/^\$document-([0-9]*)$/) || Math.Random(1,3).toString()
 		let doc = {
 			mimeType: f.mimetype,
 			size: f.size,
 			content: f.buffer,
-			link: path.join(__dirname, '../', '/public/uploads/', publicKey),
-			id: +id[1]
+			link: path.join(__dirname, '../', '/public/uploads/', publicKey, '/', id),
+			id: id
 		}
 		return doc
 	})
+	// Add docs to DB
 	documents = documents.map(doc => {
 		let newDoc = Document.create(doc)
 		return doc
 	})
+
+	// LIB?
 	attributes = attributes.map(attr => {
 		let attrDocs = attr.documents
 			.map(id => {
@@ -80,20 +82,21 @@ const createUser = async (req, res) => {
 		let { value } = selfkey.denormalizeDocumentsSchema(attr.schema, attr.data, attrDocs)
 		return { id: attr.id, value }
 	})
+	
+
 	let user = await User.findOne({selfkey_wallet: publicKey})
-	let realToken = await generateToken()
+	
+	let realToken = await generateToken() // token used for URL auth
+	
 	if (user) {
 		user = await User.update({selfkey_wallet: publicKey}, { token: realToken, attributes: attributes })
 	} else {
 		user = await User.create({selfkey_wallet: publicKey, token: realToken, attributes: attributes})
 	}
 	if (!user) {
-		return res.status(400).json({
-			code: 'could_not_create',
-			message: 'Could not create user'
-		})
+		return res.status(400).json({ code: 'could_not_create', message: 'Could not create user' })
 	}
-	return res.status(201).json()
+	return res.status(201).json({ user: user, token: realToken})
 }
 
 // API
@@ -101,19 +104,16 @@ const getUserPayload = async (req, res) => {
 	let publicKey = req.decodedAuth.sub
 	let user = await User.findOne({selfkey_wallet: publicKey})
 	if (!user) {
-		return res.status(404).json({
-			code: 'user_does_not_exist',
-			message: 'User with provided public key does not exist'
-		})
+		return res.status(404).json({ code: 'user_does_not_exist', message: 'User with provided public key does not exist' })
 	}
-	let userToken = jwt.sign({}, JWT_SECRET, { subject: '' + user.selfkey_wallet })
+	let userToken = selfkey.userJWT({ subject: '' + user.selfkey_wallet })
 	return res.status(200).json({ token: userToken })
 }
 
 // ROUTES
 
 router.get('/selfkey/v2/auth/challenge/:publicKey', async (req, res) => {
-	let cr = await selfkey.newJWT(req.params.publicKey)
+	let cr = await selfkey.challengeJWT(req.params.publicKey)
 	return res.status(cr.status).json(cr)
 })
 
